05ff4868e5044e412f914797f2ee7539cb316f7c
willmclellarn <willmclellarn@gmail.com>
2022-04-05 16:11:19 -0600

    copper door work

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {};
+
+  onSearchSubmit(term) {
+    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID CPXUoX5lXxr_4aHNzhCO4fb22UgB9OfFEXwmTdNwF2I",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
