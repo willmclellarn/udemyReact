09783b9050fdb9eacabedb8d59faf1ab3a624d12
Will <willmclellarn@gmail.com>
2022-04-05 07:02:11 -0600

    apr 5 morning sync

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const SeasonDisplay = (props) => {
+  console.log(props.lat);
+  return <div>Season Display: {props.lat}</div>;
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,40 +3,29 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  // classic constructor fn
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  // good place to do some data loading :)
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // console.log(position);
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        console.log(err);
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
-
-  // componentDidMount() {
-  //   console.log("My component was rendered to the screen.");
-  // }
 
   // componentDidUpdate() {
   //   console.log("My component was just updated.");
   // }
 
   // React says we have to define render!!
+  // just return JSX, nothing else
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && !this.state.lat) {
       return <div>Loading...</div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
   }
 }
