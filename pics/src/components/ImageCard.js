b7d6f588fc71cfbc42ebce5c458d411b7f729d8e
willmclellarn <willmclellarn@gmail.com>
2022-04-06 12:00:55 -0600

    grid done w/ refs

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 class ImageCard extends React.Component {
-  state = {};
+  state = { spans: 0 };
+
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    // console.log(this.imageRef.current.clientHeight);
+    const spans = Math.ceil(this.imageRef.current.clientHeight / 10);
+    console.log(spans);
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
